0d5e07bdd4ba181c085f3029d4525efe8757f82d
Sherry He <xsherryhe@gmail.com>
2021-10-23 17:24:03 -0400

    Created game function in rps-game.jps

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # rock-paper-scissors
 This project is a webpage that plays Rock Paper Scissors, built from CSS and HTML.
 
-The project primarily demonstrates basic programming logic and JavaScript fundamentals including variables, data types, built-in methods, functions, and conditionals.
+The project primarily demonstrates basic programming logic and JavaScript fundamentals including variables, data types, built-in methods, functions, object literals, loops, ternaries, and conditionals.
--- scripts/rps-game.js
+++ scripts/rps-game.js
@@ -1,8 +1,9 @@
-//Return 'rock', 'paper', or 'scissors' at random as computer's selection
+//Return selection of 'rock', 'paper', or 'scissors' at random as computer
 function computerPlay() {
     return ['Rock', 'Paper', 'Scissors'][Math.floor(Math.random() * 3)];
 }
 
+//Return results of a round of Rock Paper Scissors with computer
 function playRound(playerSelection, computerSelection) {
     //Define win conditions
     const WINS_OVER = {'Rock': 'Paper',
@@ -18,6 +19,42 @@ function playRound(playerSelection, computerSelection) {
         losingSelection = winOrLose == 'Win' ? computerSelection : playerSelection;
 
     //Return string with result and winning/losing selections
-    return playerSelection == computerSelection ? 'It\'s a tie!'
+    return playerSelection == computerSelection ? 'It\'s a tie! Rematch...'
          : `You ${winOrLose}! ${winningSelection} beats ${losingSelection}`;
+}
+
+//Return results of a 5-round game of Rock Paper Scissors with computer
+function game() {
+    //Create a scoreboard to keep score
+    let scoreboard = [];
+
+    //Play 5 rounds
+    for(let i = 0; i < 5; i++) {
+        //Obtain player selection from the user
+        let playerSelection = prompt('Please type your selection: Rock, Paper, or Scissors.');
+        //Exit the game if the user cancels the game during player selection
+        if(playerSelection == null) {
+            console.log('You canceled the game.'); 
+            return;
+        }
+        //While the user has input an invalid selection, continue prompting until a valid selection is input
+        while(!['rock', 'paper', 'scissors'].includes(playerSelection.toLowerCase()))
+            playerSelection = prompt('Please enter a valid selection: Rock, Paper, or Scissors.');
+
+        //Obtain computer selection from computerPlay function and initiate the round
+        let computerSelection = computerPlay(),
+            roundResult = playRound(playerSelection, computerSelection);
+
+        //Log the result of the round
+        console.log(`Round ${i + 1}: ` + roundResult);
+        
+        //If the result was a tie, rematch the round by decrementing the round number
+        if(roundResult.indexOf('tie') > 0) i--;
+        //Otherwise, record the result of the round to the scoreboard as true if player won, false if player lost
+        else scoreboard.push(roundResult.indexOf('Win') > 0);
+    }
+
+    //Log the winner of the entire game, based on the number of trues on the scoreboard
+    console.log(scoreboard.filter(roundResult => roundResult).length > 2 ?
+                'Congratulations! You Won!' : 'Sorry, You Lost.');
 }
